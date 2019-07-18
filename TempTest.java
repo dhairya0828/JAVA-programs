4d737419c765420b4946c6dce716123a93b3f23d
dhairya0828 <dhairya0828@github.com>
2019-07-18 19:53:23 +0530

    Updated content.

--- TempTest.java
+++ TempTest.java
@@ -1,1 +1,17 @@
-class TempTest{}
+class TempTest{
+
+    static String str = "KHUSHALO";
+    static String tempMethod(String str)
+    {
+        System.out.println("In Method : " + str);
+        str = "Dhairya";
+        System.out.println("After updating in Method : " + str);
+        return str;
+    }
+    public static void main(String[] args) {
+    //    String str = "Khushali";
+        System.out.println("Before calling Method : " + str);
+        tempMethod(str);
+        System.out.println("After calling Method : " + str);
+    }
+}
